4cfb9f62c1b556af838bec9af1879437f68c83ac
Zero-Amir <122447410+Zero-Amir@users.noreply.github.com>
2023-01-18 13:46:00 +0100

    Added funtion

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
+
+function test() {
+    console.log('hallo')
+}
